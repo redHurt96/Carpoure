f5172758bfc8c7c97746c3a874834f801b8f4e22
redHurt <62645756+redHurt96@users.noreply.github.com>
2021-12-27 01:05:33 +0300

    Поднастроена сцена

---
+++ Assets/_Game/Scripts/LevelObjects/UndestrictibleObjects.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace RoofRace.LevelObjects
+{
+    public class UndestrictibleObjects : MonoBehaviour
+    {
+        [SerializeField] private GameObject _explosion;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag(Tags.PLAYER))
+            {
+                LevelStateMachine.Instance.FailLevel();
+                _explosion.SetActive(true);
+            }
+        }
+    }
+}
