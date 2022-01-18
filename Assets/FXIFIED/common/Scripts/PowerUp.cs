89fe05439f3aac9b95d59ef5e9b1c1f0ff0806fe
redHurt <62645756+redHurt96@users.noreply.github.com>
2022-01-19 02:04:43 +0300

    Добавлен эффект для коллектаблов

---
+++ Assets/FXIFIED/common/Scripts/PowerUp.cs
@@ -0,0 +1,19 @@
+﻿using RoofRace;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public GameObject pickupEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(Tags.PLAYER))
+            Pickup();
+    }
+
+    private void Pickup()
+    {
+        var effect = Instantiate(pickupEffect, transform.position, transform.rotation);
+        Destroy(effect.gameObject, 3f);
+    }
+}
--- Assets/_Game/Scripts/Collectables/CollectableObject.cs
+++ Assets/_Game/Scripts/Collectables/CollectableObject.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace RoofRace.Collectables
 {
     [RequireComponent(typeof(Collider))]
     internal class CollectableObject : MonoBehaviour
     {
+        public event Action Collected;
+
         public int Value => _value;
 
         [SerializeField] private int _value = 1;
@@ -14,7 +17,10 @@ namespace RoofRace.Collectables
             if (other.CompareTag(Tags.PLAYER))
             {
                 CollectablesMaganer.Instance.AddItem(this);
-                Destroy(gameObject);
+
+                Collected?.Invoke();
+
+                Destroy(gameObject, .1f);
             }
         }
     }
