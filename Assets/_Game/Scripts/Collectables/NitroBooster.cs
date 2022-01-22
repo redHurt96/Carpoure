95e7e21047b523f2435a51b8737b099e48898ec6
redHurt <62645756+redHurt96@users.noreply.github.com>
2022-01-22 15:29:31 +0300

    нитро

---
+++ Assets/_Game/Scripts/Collectables/NitroBooster.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace RoofRace.Collectables
+{
+    public class NitroBooster : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/_Game/Scripts/Collectables/NitroManager.cs
@@ -0,0 +1,40 @@
+﻿using RH.Utilities.SingletonAccess;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace RoofRace.Collectables
+{
+    public class NitroManager : MonoBehaviourSingleton<NitroManager>
+    {
+        public event Action Enabled;
+        public event Action Disabled;
+        public event Action CountChanged;
+
+        public int Count { get; private set; }
+        public int MaxCount => _enableCount;
+
+        [SerializeField] private int _enableCount = 20;
+        [SerializeField] private float _activeTime = 3f;
+
+        private void Start() => CollectablesMaganer.Instance.CountChanged += AddNitro;
+        private void OnDestroy() => CollectablesMaganer.Instance.CountChanged -= AddNitro;
+
+        public void Enable() => StartCoroutine(NitroBoost());
+
+        private void AddNitro()
+        {
+            Count++;
+            CountChanged?.Invoke();
+        }
+
+        private IEnumerator NitroBoost()
+        {
+            Enabled?.Invoke();
+
+            yield return new WaitForSeconds(_activeTime);
+
+            Disabled?.Invoke();
+        }
+    }
+}
